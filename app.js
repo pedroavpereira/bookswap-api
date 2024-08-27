5c12fb4ff33227db762e825f7a521e0f914c0700
tiffjai <tiffanieho@outlook.com>
2024-08-27 17:39:15 +0100

    del comment

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Use the review router for routes starting with /reviews
+ 
 app.use('/reviews', reviewRouter);
 
-// Define the port number. Use environment variable PORT if available, otherwise default to 3000.
+ 
 const PORT = process.env.PORT || 3000;
 
-// Start the server and listen on the specified port
+ 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
