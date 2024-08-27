e8d8e227f3907a5bdad930520fa2d72bd4843565
Pedro pereira <pedroavpereira2000@gmail.com>
2024-08-27 21:40:43 +0100

    Add authenticator middleware

---
+++ middleware/authenticator.js
@@ -0,0 +1,16 @@
+const axios = require("axios");
+
+const authenticator = async (req, res, next) => {
+  try {
+    const response = await axios.get(
+      `${process.env.AUTH_API_URL}/validate-token`,
+      { headers: { authorization: req.headers.authorization } }
+    );
+    req.user_id = response.data.user_id;
+    next();
+  } catch (err) {
+    res.status(401).json({ error: "Route requires authentication" });
+  }
+};
+
+module.exports = { authenticator };
