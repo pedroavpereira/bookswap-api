d20ac139099f233470097e7700f72bd7228154bf
php20ai <aliah.iqbal123@hotmail.com>
2024-08-28 12:51:56 +0100

    Change order of tables in setup.sql.

--- db/setup-db.js
+++ db/setup-db.js
@@ -1,1 +1,5 @@
-db = require('')
+const fs = require('fs')
+require('dotenv').config()
+db = require('./connect')
+
+db.query(fs.readFileSync('./db/setup.sql').toString()).then(data => console.log("db setup complete")).catch(err => console.log("The errors is :", err))
