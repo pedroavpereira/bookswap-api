a5d5bcf4fad6dd22b9e2e73666aa9b8404300478
Pedro pereira <pedroavpereira2000@gmail.com>
2024-08-28 13:53:39 +0100

    Add user data to searchByUser

--- controllers/collections.js
+++ controllers/collections.js
@@ -112,10 +112,8 @@ const searchByUser = async (req, res) => {
     const results = await Promise.all(
       collection.map(async (col) => {
         const book = await Book.findById(col.book_id);
-
-        console.log(book);
-
-        return { ...col, book };
+        const user = await User.findById(col.user_id);
+        return { ...col, book, user };
       })
     );
 
