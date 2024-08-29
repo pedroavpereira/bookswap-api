9c73009134cd8892315bb831c2484c337ddcdba7
Pedro pereira <pedroavpereira2000@gmail.com>
2024-08-29 20:37:58 +0100

    Add authenticator and mine route

--- controllers/wishlist.js
+++ controllers/wishlist.js
@@ -1,37 +1,49 @@
-const Wishlist = require('../models/Wishlist');
+const Wishlist = require("../models/Wishlist");
 
 async function create(req, res) {
-    try {
-        const data = req.body;
-        const newWishlist = await Wishlist.create(data);
-        res.status(201).json(newWishlist);
-    } catch (err) {
-        console.error("Error creating review:", err);
-        res.status(400).json({ error: err.message || "An unknown error occurred" });
-    }
+  try {
+    const data = req.body;
+    const newWishlist = await Wishlist.create(data);
+    res.status(201).json(newWishlist);
+  } catch (err) {
+    console.error("Error creating review:", err);
+    res.status(400).json({ error: err.message || "An unknown error occurred" });
+  }
 }
+
 // show function:
 async function show(req, res) {
-    try {
-        const userId = parseInt(req.params.user_id);
-        const wishlists= await Wishlist.findByUserId(userId);
-        res.status(200).json(wishlists);
-    } catch (err) {
-        console.error("Error fetching wishlists:", err);
-        res.status(404).json({ error: err.message || "Wishlists not found." });
-    }
+  try {
+    const userId = parseInt(req.params.user_id);
+    const wishlists = await Wishlist.findByUserId(userId);
+    res.status(200).json(wishlists);
+  } catch (err) {
+    console.error("Error fetching wishlists:", err);
+    res.status(404).json({ error: err.message || "Wishlists not found." });
+  }
+}
+
+async function showMine(req, res) {
+  try {
+    const userId = req.user_id;
+    const wishlists = await Wishlist.findByUserId(userId);
+    res.status(200).json(wishlists);
+  } catch (err) {
+    console.error("Error fetching wishlists:", err);
+    res.status(404).json({ error: err.message || "Wishlists not found." });
+  }
 }
 
 async function destroy(req, res) {
-    try {
-        const wishlistId = parseInt(req.params.wishlist_id);
-        const wishlist= await Wishlist.findByWishlistId(wishlistId);
-        await wishlist.destroy();
-        res.status(204).end();
-    } catch (err) {
-        console.error("Error deleting wishlist:", err);
-        res.status(404).json({ error: err.message || "Wishlist not found" });
-    }
+  try {
+    const wishlistId = parseInt(req.params.wishlist_id);
+    const wishlist = await Wishlist.findByWishlistId(wishlistId);
+    await wishlist.destroy();
+    res.status(204).end();
+  } catch (err) {
+    console.error("Error deleting wishlist:", err);
+    res.status(404).json({ error: err.message || "Wishlist not found" });
+  }
 }
 
-module.exports = { create, show, destroy };
+module.exports = { create, show, destroy, showMine };
