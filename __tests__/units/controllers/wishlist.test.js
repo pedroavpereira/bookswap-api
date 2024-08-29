15ded3968a097bc43f845ff716d6240c9488c5b8
php20ai <aliah.iqbal123@hotmail.com>
2024-08-29 14:19:25 +0100

    Amend wishlist controller unit test.

--- __tests__/units/controllers/wishlist.test.js
+++ __tests__/units/controllers/wishlist.test.js
@@ -33,7 +33,7 @@ describe('Wishlist controller', () => {
 
     beforeEach(() => {
       testWishlist = {wishlist_id: 1, user_id: 1, book_id: 2, radius: 5.4 }
-      mockReq = { params: { user_id: 1 } }
+      mockReq = { params: { user_id: '1' } }
     });
 
     it('should return a wishlist with a 200 status code', async () => {
@@ -44,9 +44,14 @@ describe('Wishlist controller', () => {
       expect(Wishlist.findByUserId).toHaveBeenCalledTimes(1);
       expect(mockStatus).toHaveBeenCalledWith(200);
       expect(mockJson).toHaveBeenCalledWith([new Wishlist(testWishlist)])
+      expect(Wishlist.findByUserId).toHaveBeenCalledwith(1)
+      expect(req.status).toHaveBeenCalledWith(200)
+      expect(req.json).toHaveBeenCalledWith([new Wishlist(testWishlist.user_id, testWishlist.book_id, testWishlist.radius, wishlist_id)])
+      
     })
 
     it('should return an error if the wishlist with certain user_id is not found', async () => {
+
       jest.spyOn(Wishlist, 'findByUserId').mockRejectedValue(new Error('oh no'))
 
       await wishlistController.show(mockReq, mockRes)
@@ -54,11 +59,17 @@ describe('Wishlist controller', () => {
       expect(Wishlist.findByUserId).toHaveBeenCalledTimes(1)
       expect(mockStatus).toHaveBeenCalledWith(404)
       expect(mockJson).toHaveBeenCalledWith({ error: 'oh no' })
+      expect(Wishlist.findByUserId).toHaveBeenCalledwith(1)
+      expect(req.status).toHaveBeenCalledWith(404)
+      expect(req.json).toHaveBeenCalledWith({error: 'oh no' || "Wishlists not found" })
+      
     })
   })
 
   describe ('create', () => {
     it('should return a new wishlist with a 201 status code', async () => {
+      const req = {body: { user_id:1, book_id:101, rating:5}}
+      const res = { status: jest.fn().mockReturnThis(), json: jest.fn()}
       let testWishlist = {user_id: 1, book_id: 3, radius: 9.5 }
       const mockReq = { body: testWishlist }
       const wishlist_id = 1
@@ -70,6 +81,9 @@ describe('Wishlist controller', () => {
       expect(Wishlist.create).toHaveBeenCalledTimes(1)
       expect(mockStatus).toHaveBeenCalledWith(201)
       expect(mockJson).toHaveBeenCalledWith(new Wishlist(testWishlist.user_id, testWishlist.book_id, testWishlist.radius, wishlist_id))
+      expect(Wishlist.create).toHaveBeenCalledwith(req.body)
+      expect(req.status).toHaveBeenCalledWith(201)
+      expect(res.json).toHaveBeenCalledWith(new Wishlist(testWishlist.user_id, testWishlist.book_id, testWishlist.radius, wishlist_id))
     })
 
 
@@ -82,6 +96,8 @@ describe('Wishlist controller', () => {
       await wishlistController.create(mockReq, mockRes)
       
       expect(Wishlist.create).toHaveBeenCalledTimes(1)
+      expect(Wishlist.create).toHaveBeenCalledwith(req.body)
+      expect(req.status).toHaveBeenCalledWith(400)
       expect(mockStatus).toHaveBeenCalledWith(400)
       expect(mockJson).toHaveBeenCalledWith({ error: 'oh no' || 'Unknown error'})
       expect(console.error).toHaveBeenCalledTimes(1)
@@ -104,6 +120,8 @@ describe('Wishlist controller', () => {
       expect(Wishlist.prototype.destroy).toHaveBeenCalledTimes(1);
       expect(mockStatus).toHaveBeenCalledWith(204);
       expect(mockEnd).toHaveBeenCalled();
+      expect(req.status).toHaveBeenCalledWith(204)
+      expect(req.end).toHaveBeenCalledWith()
     });
 
     it('should return an error if the wishlist is not found', async () => {
@@ -116,6 +134,9 @@ describe('Wishlist controller', () => {
       expect(Wishlist.findByWishlistId).toHaveBeenCalledWith(49);
       expect(mockStatus).toHaveBeenCalledWith(404);
       expect(mockJson).toHaveBeenCalledWith({ error: 'Wishlist not found' });
+      expect(req.status).toHaveBeenCalledWith(404)
+      expect(req.json).toHaveBeenCalledWith({ error: 'Wishlist not found' || "Wishlist not found" })
+      
     });
   })
 })
